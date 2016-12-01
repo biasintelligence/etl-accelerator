8f8d5032de817bbf43bf6e7de40bbc5cbb186fcb
andreyshish <andreyshish@hotmail.com>
2016-12-01 14:51:56 -0800

    Upgrade SystemParameters procs

--- ETL_Framework/Database/ETL_ControllerCommon/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/prc_SystemParameterSet.proc.sql
+++ ETL_Framework/Database/ETL_ControllerCommon/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/prc_SystemParameterSet.proc.sql
@@ -7,52 +7,10 @@ CREATE PROCEDURE [dbo].[prc_SystemParameterSet] (
 /*
 ** Name:  [dbo].[prc_SystemParameterSet]
 **
-** $Workfile: prc_systemparameterset.sql $
-** $Archive: /Development/SubjectAreas/Dimensions20/Database/Schema/Procedure/prc_systemparameterset.sql $
-**
 ** Purpose:
-**      This script creates a stored procedure to replace the current system
-**  parameter setting(s) with the new setting(s).
+**      Set current systemParameters values 
 **
-** $Author: Karlj $
-** $Revision: 1 $
-** $BuildVersion: $
-**
-** Pre-conditions:
-**      The SystemParameters table must exist as well as the specified
-**  parameter.
-**
-** Input Arguments:
-**
-**      Name:         @ParameterType
-**      Datatype:     udt_ParameterType
-**      Default:      NULL
-**      Description:  The name of the system parameter category.
-**
-**      Name:         @ParameterName
-**      Datatype:     udt_ParameterName
-**      Default:      NULL
-**      Description:  The name of the system parameter to set.
-**
-**      Name:         @EnvironmentName
-**      Datatype:     VARCHAR(100)
-**      Default:      All
-**      Description:  The name of the Environment where this parameter applies.
-**                    
-**
-** Output Arguments:
-**      None.
-**
-** Return Code:
-**      0 = SUCCEED
-**      1 = FAILURE
-**
-** Results Set:
-**      None.
-**
-** Post-conditions:
-**      None.
-*/
+**/
 
 
 
@@ -67,6 +25,8 @@ CREATE PROCEDURE [dbo].[prc_SystemParameterSet] (
     DECLARE @SUCCEED int                        -- Success code for RETURN.
 
     DECLARE @ProcedureName sysname              -- This procedure.
+	DECLARE @msg nvarchar(1000)
+	DECLARE @err int;
 
     /*
     ** Initialize the @ProcedureName for error messages.
@@ -86,23 +46,24 @@ CREATE PROCEDURE [dbo].[prc_SystemParameterSet] (
     ** Make sure that the @Parameter name exists if it is not NULL.
     */
 
-    IF @ParameterName IS NOT NULL
+    IF (@ParameterName IS NOT NULL)
         IF NOT EXISTS (SELECT *
                          FROM [dbo].[SystemParameters]
                         WHERE [ParameterType] = COALESCE(@ParameterType, '')
                           AND [ParameterName] = @ParameterName
 						  AND [EnvironmentName] = @EnvironmentName)
             BEGIN
-                RAISERROR (50009, 16, -1, @ProcedureName, @ParameterName)
-                RETURN @FAIL
+				SET @msg = '@ParameterName: ' + @ParameterName + ' is not found'; 
+				THROW 50008, @msg, 1;
+				RETURN @FAIL
             END
 
     /*
     ** Set the parameter value.
     */
-
-    IF @ParameterType IS NOT NULL
-        IF @ParameterName IS NULL
+	BEGIN TRY
+    IF (@ParameterType IS NOT NULL)
+        IF (@ParameterName IS NULL)
             UPDATE [dbo].[SystemParameters]
                SET [ParameterValue_Current] = ParameterValue_New
              WHERE [ParameterType] = @ParameterType
@@ -118,10 +79,12 @@ CREATE PROCEDURE [dbo].[prc_SystemParameterSet] (
            SET [ParameterValue_Current] = ParameterValue_New
 		WHERE [EnvironmentName] = @EnvironmentName
 
-    IF @@ERROR <> 0
-        BEGIN
-            RAISERROR (50001, 16, -1, @ProcedureName, '[dbo].[SystemParameters]')
-            RETURN @FAIL
-        END
 
-    RETURN @SUCCEED
+    RETURN @SUCCEED;
+	END TRY
+	BEGIN CATCH
+		SET @msg = ERROR_MESSAGE();
+		SET @err = ERROR_NUMBER();
+		THROW @Err, @msg,1;
+		RETURN @FAIL;
+	END CATCH
