4d851ec25b134050b87957badff4b170212479c1
Andrey Shishkarev <andrey@biasintelligence.com>
2016-12-02 21:53:10 -0800

    increase ETLProcess.Param to 2048

--- ETL_Framework/Database/ETL_Controller/Schema Objects/Schemas/dbo/Tables/ETLProcess.table.sql
+++ ETL_Framework/Database/ETL_Controller/Schema Objects/Schemas/dbo/Tables/ETLProcess.table.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[ETLProcess] (
     [ProcessID] INT       IDENTITY (1, 1) NOT NULL,
     [Process]   [sysname] NOT NULL,
-    [Param]     [sysname] NULL,
+    [Param]     [nvarchar](2048) NULL,
     [ScopeID]   TINYINT   NULL,
     PRIMARY KEY CLUSTERED ([ProcessID] ASC) WITH (ALLOW_PAGE_LOCKS = ON, ALLOW_ROW_LOCKS = ON, PAD_INDEX = OFF, IGNORE_DUP_KEY = OFF, STATISTICS_NORECOMPUTE = OFF)
 );
