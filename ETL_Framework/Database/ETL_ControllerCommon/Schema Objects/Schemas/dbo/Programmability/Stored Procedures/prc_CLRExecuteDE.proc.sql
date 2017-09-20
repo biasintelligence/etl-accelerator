9011d7bf644a3af93c0eb7839db7de9e4febaccc
Andrey Shishkarev <andrey@biasintelligence.com>
2017-09-20 13:27:07 -0700

    Remove de and de clr support form db

--- ETL_Framework/Database/ETL_ControllerCommon/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/prc_CLRExecuteDE.proc.sql
+++
@@ -1,4 +0,0 @@
-﻿CREATE PROCEDURE [dbo].[prc_CLRExecuteDE]
-@exe NVARCHAR (4000), @args NVARCHAR (MAX), @timeout INT=0, @options NVARCHAR (1000)=NULL
-AS EXTERNAL NAME [DeSqlClr].[ETL_Framework.DESQLCLR.DeFunctions].[ExecuteDE]
-
