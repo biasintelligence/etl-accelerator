f7b300e40dc6c51c939526247f111b19b25dd0a4
Andrey Shishkarev <andrey@biasintelligence.com>
2018-02-03 11:15:37 -0800

    remove unused

--- ETL_Framework/PublishedReferences/TestFiles/Drop_QueryType_MDX_Tables.sql
+++
@@ -1,6 +0,0 @@
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[fct_Orders]') AND type in (N'U'))
-DROP TABLE [dbo].[fct_Orders];
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[dimMember]') AND type in (N'U'))
-DROP TABLE [dbo].[dimMember];
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[MemberTotalOrderAmount]') AND type in (N'U'))
-DROP TABLE [dbo].[MemberTotalOrderAmount];
