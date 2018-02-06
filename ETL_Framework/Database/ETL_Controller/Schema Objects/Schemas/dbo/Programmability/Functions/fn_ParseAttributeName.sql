d1897ce08a77f6f35fc3987522a60f2b6c682115
Andrey Shishkarev <andrey@biasintelligence.com>
2018-02-06 14:13:07 -0800

    fn_ParseAttributeName

---
+++ ETL_Framework/Database/ETL_Controller/Schema Objects/Schemas/dbo/Programmability/Functions/fn_ParseAttributeName.sql
@@ -0,0 +1,19 @@
+﻿CREATE FUNCTION [dbo].[fn_ParseAttributeName]
+(
+	@counterName varchar(100)
+)
+RETURNS @returntable TABLE
+(
+	partId int,
+	partValue varchar(100)
+)
+AS
+BEGIN
+	declare @tbl table (id int identity(1,1),value varchar(100));
+	insert @tbl (value)
+	output inserted.id as partId
+	,inserted.value as partValue
+	into @returntable
+	select top(3) value from string_split(@counterName,':');
+	return;
+END
